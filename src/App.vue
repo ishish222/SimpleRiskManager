dab641706c527711e2d0d96ae21d2f0e5a913950
korrino <tomasz.salacinski@korrino.com>
2023-03-21 21:02:49 +0100

    Added auth

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <authenticator :social-providers="['google']">
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { Authenticator } from '@aws-amplify/ui-vue'
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 export default {
   name: 'App',
